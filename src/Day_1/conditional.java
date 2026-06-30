cc72d697b17aeb2add85da3c1ae0cc04eb8648a0
Vijayyy <vboopathi600@gmail.com>
2026-06-30 11:21:05 +0530

    if else

---
+++ src/Day_1/conditional.java
@@ -0,0 +1,22 @@
+package Day_1;
+
+import java.util.Scanner;
+public class conditional {
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number:");
+            int n=sc.nextInt();
+
+
+            if(n>90){
+                System.out.println("Greaterthan 90");
+            }
+            else if(n>50){
+                System.out.println("Greaterthan 50");
+            }
+            else{
+                System.out.println("Greaterthan 10");
+            }
+        }
+
+}
